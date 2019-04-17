b85084210316c9ca102947a3ab0ad381f96ac280
Tristen Rivera <trivera1@my.westga.edu>
2019-04-17 18:46:04 -0400

    Test

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include "WordScrambleWindow.h"
+using namespace view;
+
+int main (int argc, char ** argv)
+{
+    WordScrambleWindow mainWindow(540, 375, "A4: Word Scramble by Group C");
+    mainWindow.show();
+
+    int exitCode = Fl::run();
+    return exitCode;
+}
+
+
+
+
+
+
+
+
---
+++ view/WordScrambleWindow.cpp
@@ -0,0 +1,16 @@
+#include "WordScrambleWindow.h"
+
+namespace view
+{
+
+WordScrambleWindow::WordScrambleWindow(int width, int height, const char* title) : Fl_Window(width, height, title)
+{
+    //ctor
+}
+
+WordScrambleWindow::~WordScrambleWindow()
+{
+    //dtor
+}
+
+}
