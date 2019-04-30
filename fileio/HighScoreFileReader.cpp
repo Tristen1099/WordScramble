f21991d12d567d2238812a46aef357f83f8f3f04
Tristen Rivera <trivera1@my.westga.edu>
2019-04-30 06:27:56 -0400

    Added highScore class to make it eaiser to keep track and sort all scores

--- fileio/HighScoreFileReader.cpp
+++ fileio/HighScoreFileReader.cpp
@@ -22,7 +22,45 @@ void HighScoreFileReader::loadFile()
         while ( getline (myfile,line) )
         {
 
+size_t pos = 0;
+            string score;
+            string timeLimit;
+            string date;
 
+            for(int index=0; index<3; index++)
+            {
+                string token = "";
+                pos = line.find(",");
+                token = line.substr(0, pos);
+                if(index==0)
+                {
+                    score = token;
+                }
+                else if(index==1)
+                {
+
+                    timeLimit = token;
+                }
+                else
+                {
+                    date = token;
+                }
+                line.erase(0, pos + 1);
+
+            }
+
+            try
+            {
+                int value = stoi(score);
+                HighScore newScore;
+                newScore.setHighScore(value);
+                newScore.setDate(date);
+                newScore.setTimeLimit(timeLimit);
+                this->scores.push_back(newScore);
+            }
+            catch(...)
+            {
+            }
 
         }
         myfile.close();
@@ -32,7 +70,7 @@ void HighScoreFileReader::loadFile()
 
 }
 
-vector<string> HighScoreFileReader::getScores()
+vector<HighScore> HighScoreFileReader::getScores()
 {
 
     return this->scores;
